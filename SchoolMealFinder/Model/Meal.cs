a19ab937e9234df7b478f1aa49a5e4878688fc19
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-08 19:15:41 +0900

    add Models

---
+++ SchoolMealFinder/Model/Meal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMealFinder.Model
+{
+    class Meal : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private List<string> menuInfos;
+
+        private List<Review>
+    }
+}
---
+++ SchoolMealFinder/Model/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMealFinder.Model
+{
+    class Review : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private int score;
+
+        private string id;
+
+        private string name;
+    }
+}
