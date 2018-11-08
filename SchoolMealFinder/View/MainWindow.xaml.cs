e7ac75205b8ead61d54e8bddc717de41e200028f
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-08 10:23:00 +0900

    add Sha512Hash(string str)

--- SchoolMealFinder/View/MainWindow.xaml.cs
+++ SchoolMealFinder/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SchoolMealFinder
+namespace SchoolMealFinder.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +24,30 @@ namespace SchoolMealFinder
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string Sha512Hash(string str)
+        {
+            var sha512 = new SHA512CryptoServiceProvider();
+            byte[] resultHash = sha512.ComputeHash(Encoding.Default.GetBytes(str));
+            string transPwd = string.Empty;
+
+            foreach (byte x in resultHash)
+            {
+                transPwd += $"{x:x2}";
+            }
+
+            return transPwd;
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(idTb.Text == "" || pwPb.Password == "")
+            {
+                MessageBox.Show("아이디 또는 비밀번호를 입력하세요.", "Error!");
+            }
+
+
         }
     }
 }
