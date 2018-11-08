bae78c8b416ebc8c2fff3590dcd203fb2dfe91b5
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-08 19:16:14 +0900

    login

---
+++ SchoolMealFinder/Model/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMealFinder.Model
+{
+    class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private string id;
+        public string Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+
+        private int auth;
+        public int Auth
+        {
+            get => auth;
+            set
+            {
+                auth = value;
+            }
+        }
+
+        private int schoolIdx;
+        public int SchoolIdx
+        {
+            get => schoolIdx;
+            set
+            {
+                schoolIdx = value;
+
+            }
+        }
+
+        private int 
+
+    }
+}
--- SchoolMealFinder/View/MainWindow.xaml.cs
+++ SchoolMealFinder/View/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -8,7 +11,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Input; 
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,6 +24,9 @@ namespace SchoolMealFinder.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string sqlStr = ConfigurationManager.AppSettings["DBConn"];
+        private string userName = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -44,10 +50,25 @@ namespace SchoolMealFinder.View
         {
             if(idTb.Text == "" || pwPb.Password == "")
             {
-                MessageBox.Show("아이디 또는 비밀번호를 입력하세요.", "Error!");
+                MessageBox.Show("아이디 또는 비밀번호를 입력하세요.", "알림", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
+            var Conn = new MySqlConnection(sqlStr);
+            Conn.Open();
 
+            var Comm = new MySqlCommand("select name from user where id='" + idTb.Text + "' and pw='" + Sha512Hash(pwPb.Password.ToString()) + "'", Conn);
+            var MyRead = Comm.ExecuteReader();
+
+            if (MyRead.Read())
+            {
+                userName = MyRead["name"].ToString();
+                MessageBox.Show(userName + "님 환영합니다!", "알림", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("로그인에 실패하였습니다.", "알림", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+
     }
 }
