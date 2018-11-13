907e43c35b7fc9e73a0acb0efb6d63c050217362
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-13 14:02:18 +0900

    MysqlConn 으로 모듈화

---
+++ SchoolMealFinder/DBConn/MysqlConn.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SchoolMealFinder.DBConn
+{
+    public class MysqlConn
+    {
+        public static MySqlCommand SendQuery(string query)
+        {
+            string sqlStr = ConfigurationManager.AppSettings["DBConn"];
+
+            var Conn = new MySqlConnection(sqlStr);
+            Conn.Open();
+
+            return new MySqlCommand(query, Conn);
+        }
+}
+}
