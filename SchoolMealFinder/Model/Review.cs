95767e4951fbec05334139dfa816d89b1aa788c5
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-18 22:59:48 +0900

    public class...

--- SchoolMealFinder/Model/Review.cs
+++ SchoolMealFinder/Model/Review.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 
 namespace SchoolMealFinder.Model
 {
-    internal class Review : INotifyPropertyChanged
+    public class Review : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
