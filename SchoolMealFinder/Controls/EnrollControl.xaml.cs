a8fb961ccf3c38d84034420a0f8c73efa4b24fb3
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-19 11:32:52 +0900

    EnrollCtrl

--- SchoolMealFinder/Controls/EnrollControl.xaml.cs
+++ SchoolMealFinder/Controls/EnrollControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,23 @@ namespace SchoolMealFinder.Controls
         public EnrollControl()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tempTb.Text = "";
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] temp = tempTb.Text.ToString().Split(',');
+            for(int i=0; i<temp.Length; i++)
+            {
+                Debug.WriteLine(temp[i]);
+                //이부분에서 DB Query 날려줘야됨.
+            }
+            this.Visibility = Visibility.Collapsed;
         }
     }
 }
